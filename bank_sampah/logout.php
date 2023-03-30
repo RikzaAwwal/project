7c90152f8cb43681fc765bb4796ec7c50138edf8
RikzaAwwal <129252239+RikzaAwwal@users.noreply.github.com>
2023-03-30 15:32:44 +0700

    Delete bank_sampah directory

--- bank_sampah/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	session_start();
-
-	session_destroy();
-
-	setcookie('login', '', time()-1);
-
-	header("Location: login.php");
-?>
--- bank_sampah/test.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	require 'fungsi.php';
-	$f="082262656527";
-	$enkripsi = enkripsi($f);
-	echo $enkripsi;
-
-	$dekripsi = dekripsi($enkripsi);
-	echo $dekripsi;
- ?>
